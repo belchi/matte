a9f1b9a16742955e4a9a88d7694e73860f82d565
Martin Belchatowski <mbelchatowski@spotify.com>
2023-07-23 10:22:28 +0200

    Updates

---
+++ precache-manifest.dfdb2d373c51c8e9f3b571f2631a240e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd54c9797b0d0b7fb50ddc7db664f414",
+    "url": "/matte/index.html"
+  },
+  {
+    "revision": "dd4c2ed09418f913c8b2",
+    "url": "/matte/static/css/2.68cf386b.chunk.css"
+  },
+  {
+    "revision": "0abba824bf318cfa9e3d",
+    "url": "/matte/static/css/main.d841a619.chunk.css"
+  },
+  {
+    "revision": "dd4c2ed09418f913c8b2",
+    "url": "/matte/static/js/2.94ae1af3.chunk.js"
+  },
+  {
+    "revision": "0abba824bf318cfa9e3d",
+    "url": "/matte/static/js/main.2a76e3af.chunk.js"
+  },
+  {
+    "revision": "4fea67e457157604015d",
+    "url": "/matte/static/js/runtime-main.4a1c7949.js"
+  }
+]);
